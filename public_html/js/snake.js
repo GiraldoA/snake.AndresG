d4e87fed7e89e3cf2c43456c9e35c444334f3c88
Andres Giraldo <actorgiraldo@gmail.com>
2014-10-10 14:01:01 -0700

    new update

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,5 +1,44 @@
 var snake;
+var snakeLength;
+var snakeSize;
 
 var conntext;
 var screenwidth;
-var screenhight;
+var screenheight;
+
+gameInitialize();
+gameDraw();
+
+function gameInitialize() {
+    var canvas = document.getElementById("game-screen");
+    context = canvas.getContext("2d");
+    
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
+}
+
+function gameLoop() {
+    
+}
+
+function gameDraw() {
+    context.fillStyle = "rgb(105, 157, 255)";
+    context.fillRect(0, 0, screenWidth, screenHeight);
+}  
+
+function snakeInitialize() {
+    snake = [];
+    snakeLength = 15;
+    snakeSize = 20;
+}
+
+function snakeDraw() {
+    
+}
+
+function snakeUpdate() {
+    
+}
