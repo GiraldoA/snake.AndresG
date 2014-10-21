c0eeafcfdd6ca7b5653de16d651d9ef0edfec7b9
Andres Giraldo <actorgiraldo@gmail.com>
2014-10-21 14:02:38 -0700

    new update

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -22,7 +22,7 @@ var screenheight;
 gameInitialize();
 snakeInitialize();
 foodInitialize();
-setInterval(gameLoop, 1000/50);
+setInterval(gameLoop, 1000/30);
 
 /*-----------------------------------------------------------------------------
  * game functions
@@ -87,8 +87,14 @@ function snakeUpdate() {
     if(snakeDirection == "down"){
         snakeHeadY++;
     }
-    else {
+    else if(snakeDirection == "right"){
         snakeHeadX++;
+    }
+    else if(snakeDirection == "left"){
+        snakeHeadX--;
+    }
+    else if(snakeDirection == "up") {
+        snakeHeadY--;
     }
     
     var snakeTail = snake.pop();
@@ -112,17 +118,35 @@ function foodInitialize() {
 
 function foodDraw() {
     context.fillStyle = "white";
-    context.fillRect(food.x, food.y, snakeSize, snakeSize);
+    context.fillRect(food.x * snakeSize, food.y * snakeSize, snakeSize, snakeSize);
 }
 
 function setFoodPosition() {
     var randomX = Math.floor(Math.random() * screenWidth); 
     var randomY = Math.floor(Math.random() * screenHeight);
     
-    food.x = randomX;
-    food.y = randomY;
+    food.x = Math.floor(randomX / snakeSize);
+    food.y = Math.floor(randomY / snakeSize);
 }
 
+/*-----------------------------------------------------------------------------
+ * input functions
+ * ----------------------------------------------------------------------------
+ */
+
 function keyboardHandler(event) {
-    console.log(event)
+    console.log(event);
+    
+    if(event.keyCode == "39" && snakeDirection != "left") {
+        snakeDirection = "right";
+    }
+    else if(event.keyCode == "40" && snakeDirection != "up"){
+        snakeDirection = "down";
+    }
+    else if(event.keyCode == "37" && snakeDirection != "right"){
+        snakeDirection = "left";
+    }
+    else if(event.keyCode == "38" && snakeDirection != "down") {
+        snakeDirection = "up";
+    }
 }
